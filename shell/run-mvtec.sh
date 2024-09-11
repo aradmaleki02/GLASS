360d0e25f6740daf81b1d8ddcfee7bf6cbeaef4a
Arad Maleki <arad.m.t02@gmail.com>
2024-09-11 06:16:42 +0330

    update

--- shell/run-mvtec.sh
+++ shell/run-mvtec.sh
@@ -16,7 +16,7 @@ python main.py \
     --pretrain_embed_dimension 1536 \
     --target_embed_dimension 1536 \
     --patchsize 3 \
-    --meta_epochs 1 \
+    --meta_epochs 50 \
     --eval_epochs 1 \
     --dsc_layers 2 \
     --dsc_hidden 1024 \
